6661c922804f97ebe8535ae78e28c2c4d11a0238
Ira <sitronenelon@gmail.com>
2023-03-11 22:10:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 570;
+        int addAmount = 1024;
+        int total = (balance + addAmount);
+
+        int bonus;
+        if (total >= 1000) {
+            bonus = (addAmount / 100);
+            total = total + bonus;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Итоговый баланс: " + total);
+        System.out.println("Бонусы: " + bonus);
+    }
+
+}
